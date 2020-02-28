5cf6a53a178de9b0a7788d108a88120827679653
alexmjn <a.jenkneary@gmail.com>
2020-02-28 09:57:11 -0500

    environment variables

--- web_app/app.py
+++ web_app/app.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify, render_template, request
 # pylint: disable=import-error
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+import os
+from dotenv import load_dotenv
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stat_routes import stat_routes
+
+load_dotenv()
 
 def create_app():
 
